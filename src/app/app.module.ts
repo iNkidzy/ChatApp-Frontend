47ad57b507c83ea51eb6c9a374adc9e69ebe8942
iNKidzy <nadq.nadia@gmail.com>
2021-03-19 00:25:54 +0100

    Material install

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {SocketIoConfig, SocketIoModule} from 'ngx-socket-io';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 const config: SocketIoConfig = { url: 'http://localhost:3600', options: {} };
 
 
@@ -16,7 +17,8 @@ const config: SocketIoConfig = { url: 'http://localhost:3600', options: {} };
     BrowserModule,
     AppRoutingModule,
     SocketIoModule.forRoot(config),
-    NgbModule
+    NgbModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
